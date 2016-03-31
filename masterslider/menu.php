7b819f2b11134525d09da8bbb2fbb645c3d5a6d7
varCloud <sapitopicador@gmail.com>
2016-03-31 17:12:55 -0600

    123

---
+++ masterslider/menu.php
@@ -0,0 +1,119 @@
+  <div class="col-md-12 nopadding section-white">
+      <div id="header">
+	    <div class="container">
+	      <div class="navbar brown-2 navbar-default yamm">
+	        <div class="navbar-header">
+	          <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle two three"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+	          <a href="index.html" class="navbar-brand"><img src="images/logo/logo.png" alt=""></a> </div>
+	        <div id="navbar-collapse-grid" class="navbar-collapse collapse pull-right">
+	          <ul class="nav navbar-nav">
+	            <li class="dropdown"> <a href="index.html" class="dropdown-toggle active">Inicio</a></li>
+	            <li class="dropdown"> <a href="index.html" class="dropdown-toggle">Galeria</a>
+					<ul class="dropdown-menu" role="menu">
+							<li><a href="http://codelayers.net/templates/hostlinea/creative/index.html">Seguridad</a> </li>
+							<li><a href="http://codelayers.net/templates/hostlinea/creative-2/index.html">Equipo de Computo</a></li>
+							<li><a href="http://codelayers.net/templates/hostlinea/classic/index.html">Instalaciones</a> </li>
+							<li><a href="index.html">Otros</a> </li>
+					</ul>
+            </li>
+	            <li> <a href="domain-search.html" class="dropdown-toggle">Dominios</a></li>
+                <li> <a href="web-hosting.html" class="dropdown-toggle">Mision</a></li>
+				<li> <a href="web-hosting.html" class="dropdown-toggle">Vision</a></li>
+				
+                
+                <li class="dropdown yamm-fw"> <a href="about2.html" class="dropdown-toggle">Productos</a>
+              <ul class="dropdown-menu">
+                <li> 
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p>Ensamble</p>
+                        </li>
+                        <li><a href="about.html"><i class="fa fa-angle-right"></i> &nbsp; Ensamble</a></li>
+                        <li><a href="about-2.html"><i class="fa fa-angle-right"></i> &nbsp; Computadoras</a></li>
+                        <li><a href="about-3.html"><i class="fa fa-angle-right"></i> &nbsp; About Style 3</a></li>
+                        <li><a href="website-builder.html"><i class="fa fa-angle-right"></i> &nbsp; Accesorios</a></li>
+                        <li><a href="affiliates.html"><i class="fa fa-angle-right"></i> &nbsp; Sonido</a></li>
+                        <li><a href="left-nav.html"><i class="fa fa-angle-right"></i> &nbsp; Left Navigation</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Pages 2 </p>
+                        </li>
+                        <li><a href="services.html"><i class="fa fa-angle-right"></i> &nbsp; Services Style 1</a></li>
+                        <li><a href="services-2.html"><i class="fa fa-angle-right"></i> &nbsp; Services Style 2</a></li>
+                        <li><a href="services-3.html"><i class="fa fa-angle-right"></i> &nbsp; Services Style 3</a></li>
+                        <li><a href="whois.html"><i class="fa fa-angle-right"></i> &nbsp; Search Whois</a></li>
+                        <li><a href="team.html"><i class="fa fa-angle-right"></i> &nbsp; Team</a></li>
+                        <li><a href="faq.html"><i class="fa fa-angle-right"></i> &nbsp; FAQ</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Pages 3 </p>
+                        </li>
+                        <li><a href="full-width.html"><i class="fa fa-angle-right"></i> &nbsp; Full Width Page</a></li>
+                        <li><a href="left-sidebar.html"><i class="fa fa-angle-right"></i> &nbsp; Left Sidebar Page</a></li>
+                        <li><a href="right-sidebar.html"><i class="fa fa-angle-right"></i> &nbsp; Right Sidebar Page</a></li>
+                        <li><a href="packages.html"><i class="fa fa-angle-right"></i> &nbsp; Package Plans</a></li>
+                        <li><a href="careers.html"><i class="fa fa-angle-right"></i> &nbsp; Careers</a></li>
+                        <li><a href="coming-soon.html"><i class="fa fa-angle-right"></i> &nbsp; Coming Soon</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Pages 4 </p>
+                        </li>
+                        <li><a href="login.html"><i class="fa fa-angle-right"></i> &nbsp; Login Form</a></li>
+                        <li><a href="register.html"><i class="fa fa-angle-right"></i> &nbsp; Registration Form</a></li>
+                        <li><a href="sitemap.html"><i class="fa fa-angle-right"></i> &nbsp; Sitemap</a></li>
+                        <li><a href="maintenance.html"><i class="fa fa-angle-right"></i> &nbsp; Maintenance</a></li>
+                        <li><a href="404.html"><i class="fa fa-angle-right"></i> &nbsp; 404 Error Page</a></li>
+                        <li><a href="404-2.html"><i class="fa fa-angle-right"></i> &nbsp; 404 Error Page2</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+            
+	            <li class="dropdown"> <a href="portfolio-three.html" class="dropdown-toggle">Portfolio</a>
+              <ul class="dropdown-menu" role="menu">
+                <li> <a href="portfolio-one.html">Single Item</a> </li>
+                <li> <a href="portfolio-two.html">Portfolio Columns 2</a> </li>
+                <li> <a href="portfolio-three.html">Portfolio Columns 3</a> </li>
+                <li> <a href="portfolio-four.html">Portfolio Columns 4</a> </li>
+                <li> <a href="portfolio-five.html">Portfolio + Sidebar</a> </li>
+                <li> <a href="portfolio-six.html">Portfolio Masonry</a> </li>
+                <li> <a href="portfolio-seven.html">Slider Projects</a> </li>
+                <li class="dropdown-submenu mul"> <a tabindex="-1" href="#">Sub Menu + </a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Menu Item 1</a></li>
+                    <li><a href="#">Menu Item 2</a></li>
+                    <li><a href="#">Menu Item 3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+	            <li class="dropdown"> <a href="blog.html" class="dropdown-toggle">Blog</a>
+              <ul class="dropdown-menu three" role="menu">
+                <li> <a href="blog.html">Blog Full Width</a> </li>
+                <li> <a href="blog-2.html">Blog 3Columns</a> </li>
+                <li> <a href="blog-3.html">Blog Standard</a> </li>
+                <li> <a href="blog-post.html">Single Post</a> </li>
+              </ul>
+            </li>
+            
+            <li class="dropdown"> <a href="contact.html" class="dropdown-toggle align-1">Contactanos</a>
+              <ul class="dropdown-menu align-1 two" role="menu">
+                <li> <a href="contact.html">Contact Variation 1</a> </li>
+                <li> <a href="contact-2.html">Contact Variation 2</a> </li>
+                <li> <a href="contact-3.html">Contact Variation 3</a> </li>
+              </ul>
+            </li>
+            
+	          </ul>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
---
+++ menu.php
@@ -0,0 +1,115 @@
+  <div class="col-md-12 nopadding section-white">
+      <div id="header">
+	    <div class="container">
+	      <div class="navbar brown-2 navbar-default yamm">
+	        <div class="navbar-header">
+	          <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle two three"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+	          <a href="index.php" class="navbar-brand"><img src="images/logo/logo.png" alt=""></a> </div>
+	        <div id="navbar-collapse-grid" class="navbar-collapse collapse pull-right">
+	          <ul class="nav navbar-nav">
+	            <li class="dropdown"> <a href="index.html" class="dropdown-toggle active">Inicio</a></li>
+	            <li class="dropdown"> <a href="index.html" class="dropdown-toggle">Galeria</a>
+					<ul class="dropdown-menu" role="menu">
+							<li><a href="http://codelayers.net/templates/hostlinea/creative/index.html">Seguridad</a> </li>
+							<li><a href="http://codelayers.net/templates/hostlinea/creative-2/index.html">Equipo de Computo</a></li>
+							<li><a href="http://codelayers.net/templates/hostlinea/classic/index.html">Instalaciones</a> </li>
+							<li><a href="index.html">Otros</a> </li>
+					</ul>
+            </li>
+	            <li> <a href="domain-search.html" class="dropdown-toggle">Dominios</a></li>
+                <li> <a href="web-hosting.html" class="dropdown-toggle">Mision</a></li>
+				<li> <a href="web-hosting.html" class="dropdown-toggle">Vision</a></li>
+				
+                
+                <li class="dropdown yamm-fw"> <a href="about2.html" class="dropdown-toggle">Productos</a>
+              <ul class="dropdown-menu">
+                <li> 
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p>Ensamble</p>
+                        </li>
+                        <li><a href="about.html"><i class="fa fa-angle-right"></i> &nbsp; Gabinetes</a></li>
+                        <li><a href="about-2.html"><i class="fa fa-angle-right"></i> &nbsp; Tarjetas Madre</a></li>
+                        <li><a href="about-3.html"><i class="fa fa-angle-right"></i> &nbsp; Procesadores</a></li>
+                        <li><a href="website-builder.html"><i class="fa fa-angle-right"></i> &nbsp; Memorias</a></li>
+                        <li><a href="affiliates.html"><i class="fa fa-angle-right"></i> &nbsp; Discos Duros</a></li>
+                        <li><a href="left-nav.html"><i class="fa fa-angle-right"></i> &nbsp; SSD</a></li>
+                        <li><a href="left-nav.html"><i class="fa fa-angle-right"></i> &nbsp; Unidades Opticas</a></li>
+                        <li><a href="left-nav.html"><i class="fa fa-angle-right"></i> &nbsp; Tarjetas de Video</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Computadoras </p>
+                        </li>
+                        <li><a href="services.html"><i class="fa fa-angle-right"></i> &nbsp; Servidores</a></li>
+                        <li><a href="services-2.html"><i class="fa fa-angle-right"></i> &nbsp; Laptops</a></li>
+                        <li><a href="services-3.html"><i class="fa fa-angle-right"></i> &nbsp; UltraBook</a></li>
+                        <li><a href="whois.html"><i class="fa fa-angle-right"></i> &nbsp; Escritorio</a></li>
+                        <li><a href="team.html"><i class="fa fa-angle-right"></i> &nbsp; All in One</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Almacenamiento Externo</p>
+                        </li>
+                        <li><a href="full-width.html"><i class="fa fa-angle-right"></i> &nbsp; Memoria Usb</a></li>
+                        <li><a href="left-sidebar.html"><i class="fa fa-angle-right"></i> &nbsp; Micro SD</a></li>
+                        <li><a href="right-sidebar.html"><i class="fa fa-angle-right"></i> &nbsp; Discos Externos</a></li>
+                      </ul>
+                      <ul class="col-sm-6 col-md-3 list-unstyled ">
+                        <li>
+                          <p> Accesorios </p>
+                        </li>
+                        <li><a href="login.html"><i class="fa fa-angle-right"></i> &nbsp; Teclados</a></li>
+                        <li><a href="register.html"><i class="fa fa-angle-right"></i> &nbsp; Mouse </a></li>
+                        <li><a href="sitemap.html"><i class="fa fa-angle-right"></i> &nbsp; Conexion</a></li>
+                        <li><a href="maintenance.html"><i class="fa fa-angle-right"></i> &nbsp; Web Cam</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+            
+	            <li class="dropdown"> <a href="portfolio-three.html" class="dropdown-toggle">Portfolio</a>
+              <ul class="dropdown-menu" role="menu">
+                <li> <a href="portfolio-one.html">Single Item</a> </li>
+                <li> <a href="portfolio-two.html">Portfolio Columns 2</a> </li>
+                <li> <a href="portfolio-three.html">Portfolio Columns 3</a> </li>
+                <li> <a href="portfolio-four.html">Portfolio Columns 4</a> </li>
+                <li> <a href="portfolio-five.html">Portfolio + Sidebar</a> </li>
+                <li> <a href="portfolio-six.html">Portfolio Masonry</a> </li>
+                <li> <a href="portfolio-seven.html">Slider Projects</a> </li>
+                <li class="dropdown-submenu mul"> <a tabindex="-1" href="#">Sub Menu + </a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Menu Item 1</a></li>
+                    <li><a href="#">Menu Item 2</a></li>
+                    <li><a href="#">Menu Item 3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+	            <li class="dropdown"> <a href="blog.html" class="dropdown-toggle">Blog</a>
+              <ul class="dropdown-menu three" role="menu">
+                <li> <a href="blog.html">Blog Full Width</a> </li>
+                <li> <a href="blog-2.html">Blog 3Columns</a> </li>
+                <li> <a href="blog-3.html">Blog Standard</a> </li>
+                <li> <a href="blog-post.html">Single Post</a> </li>
+              </ul>
+            </li>
+            
+            <li class="dropdown"> <a href="contact.html" class="dropdown-toggle align-1">Contactanos</a>
+              <ul class="dropdown-menu align-1 two" role="menu">
+                <li> <a href="contact.html">Contact Variation 1</a> </li>
+                <li> <a href="contact-2.html">Contact Variation 2</a> </li>
+                <li> <a href="contact-3.html">Contact Variation 3</a> </li>
+              </ul>
+            </li>
+            
+	          </ul>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
